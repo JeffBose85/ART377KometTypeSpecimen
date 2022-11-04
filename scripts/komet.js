e4deb3d3ad2d0be43700eee4ec6bb5a1c53e25bf
JeffreyBose <jbose8500@gmail.com>
2022-11-04 01:34:53 -0500

    Finished Experiments + Many Responsive Fixes + Animations

---
+++ scripts/komet.js
@@ -0,0 +1,40 @@
+
+
+const weights = ['100','300','400','500','700','800','900']
+let aztext;
+let customtext;
+let weightbtns;
+
+let azelement
+let weightstyle
+
+function StartUp() {
+    aztext = document.getElementById("AZ");
+    customtext = document.getElementById("customtext");
+    weightbtns = document.getElementsByClassName('weightbtn');
+    azelement = document.querySelector('#AZ');
+    weightstyle = getComputedStyle(azelement);
+
+    for(let i = 0; i<weightbtns.length;i++) {
+        console.log("Setting weight: " + weights[i]);
+        weightbtns[i].style.fontWeight = weights[i];
+    }
+
+}
+
+function ChangeAZWeight(weight) {
+    aztext.style.fontWeight = weight;
+    customtext.style.fontWeight = weight;
+}
+
+function ChangeAZWeightLeft() {
+    let index = weights.indexOf(weightstyle.fontWeight);
+    aztext.style.fontWeight = weights[index - 1];
+}
+
+function ChangeAZWeightRight() {
+    let index = weights.indexOf(weightstyle.fontWeight);
+    aztext.style.fontWeight = weights[index + 1];
+}
+
+
